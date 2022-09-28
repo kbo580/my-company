fe2939b8d5cd0d527b95245b4aed8cd4dcd9d6f1
kbo580 <kobayuta1025@gmail.com>
2022-09-28 11:12:25 +0900

    workのアコーディオンの完成

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,26 @@
 const mySwiper = new Swiper('.swiper', {
-  slidesPerView: 3,
-  spaceBetween: 100,
-  initialSlide: 2,
   loop: true,
-  centeredSlides: true,
   effect: "card",
-  autoplay: {
-          delay: 30000,
-      },
+  // autoplay: {
+  //   delay: 30000,
+  // },
+
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    // 552px以上の場合
+    552: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // 768px以上の場合
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 80,
+      centeredSlides: true,
+    }
+  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
